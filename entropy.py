53a176100359019a76ecbe064e7fc3f24497c1bc
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2023-11-29 08:19:32 +0100

    Add epsilon to avoid log(0)

--- entropy.py
+++ entropy.py
@@ -1,25 +1,26 @@
 import math
 import numpy as np
 import sys
+EPS = 1e-10
 def HY(p):
     ret=0.0
     for i in range(p.shape[0]):
         for j in range(p.shape[1]):
-            ret -= p[i,j] * math.log2(np.sum(p[:,j]))
+            ret -= p[i,j] * math.log2(np.sum(p[:,j])+EPS)
     return ret
 
 def HX(p):
     ret=0.0
     for i in range(p.shape[0]):
         for j in range(p.shape[1]):
-            ret -= p[i,j] * math.log2(np.sum(p[i,:]))
+            ret -= p[i,j] * math.log2(np.sum(p[i,:])+EPS)
     return ret
 
 def HXY(p):
     ret=0.0
     for i in range(p.shape[0]):
         for j in range(p.shape[1]):
-            ret -= p[i,j] * math.log2(p[i,j])
+            ret -= p[i,j] * math.log2(p[i,j]+EPS)
     return ret
 
 def HX_g_Y(p):
@@ -27,7 +28,7 @@ def HX_g_Y(p):
     for i in range(p.shape[0]):
         for j in range(p.shape[1]):
             pi_g_j = p[i,j]/np.sum(p[i,:])
-            ret -= p[i,j] * math.log2(pi_g_j)
+            ret -= p[i,j] * math.log2(pi_g_j+EPS)
     return ret
     
 
@@ -36,7 +37,7 @@ def HY_g_X(p):
     for i in range(p.shape[0]):
         for j in range(p.shape[1]):
             pj_g_i = p[i,j]/np.sum(p[:,j])
-            ret -= p[i,j] * math.log2(pj_g_i)
+            ret -= p[i,j] * math.log2(pj_g_i+EPS)
     return ret
 
 def I(p):
